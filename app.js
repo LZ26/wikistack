f321648a6cfa02a46c7f965bb886f73db96799f6
John Cook <jpcook72@gmail.com>
2020-08-04 20:25:30 -0500

    Updates

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override')
 const app = express();
+// const { db } = require('./models');
 
 
 app.use(morgan("dev"));
-
-
 
 app.use(express.static(__dirname + "/public"));
 
@@ -16,8 +15,13 @@ app.use(methodOverride('_method'));
 
 app.use('/', require('./routes/routes'));
 
+// db.authenticate().
+// then(() => {
+//   console.log('connected to the database');
+// })
 
-const PORT = 8080;
+
+const PORT = 5432;
 
 app.listen(PORT, () => {
   console.log('started');
